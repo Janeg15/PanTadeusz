204e81d96d6a80d2243e2790ae3e80dc61e26915
Janeg15 <15864@eans-nt.edu.pl>
2026-04-08 20:39:38 +0200

    Zamiana plików html na php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include('header.php'); ?>
+
+<main class="container-fluid">
+    <div class="row">
+        <?php include('sidebar.php'); ?>
+
+        <section class="col-md-9 col-lg-10 p-5 text-center">
+            <div class="mb-5">
+                <h2>Witaj w cyfrowej wersji epopei</h2>
+                <p class="text-muted">Wybierz księgę z menu po lewej stronie, aby rozpocząć czytanie.</p>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <img src="pantadeo.jpg" alt="Obrazek PanTadeo" class="hero-image mb-4">
+                    
+                    <div class="text-start bg-light p-4 border-start border-4 border-primary">
+                        <p class="fst-italic">
+                            "Litwo! Ojczyzno moja! ty jesteś jak zdrowie.<br>
+                            Ile cię trzeba cenić, ten tylko się dowie,<br>
+                            Kto cię stracił. Dziś piękność twą w całej ozdobie<br>
+                            Widzę i opisuję, bo tęsknię po tobie."
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+<?php include('footer.php'); ?>
