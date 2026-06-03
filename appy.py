2be76d257e097724979e60625df82d4f45a60112
Janeg15 <15864@eans-nt.edu.pl>
2026-06-03 18:17:13 +0200

    Zmiana wyświetlania ksiąg

--- appy.py
+++ appy.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/book/<int:book_id>')
+def book(book_id):
+    return render_template(f'k{book_id}.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
